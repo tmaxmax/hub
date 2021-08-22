04bb7dc637dc9faa658f71787ee93b8f61d4399c
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-08-23 01:26:45 +0300

    Write documentation

--- hub.go
+++ hub.go
@@ -1,10 +1,43 @@
 package hub
 
 type (
-	Hub         chan interface{}
-	Topic       chan interface{}
-	Conn        chan interface{}
-	Disconnect  Conn
+	// The Hub channel is used for registering and unregistering topics.
+	// Closing this channel will close all topics and connections.
+	//
+	//   hub := New()
+	//   topic := make(Topic) // you can use a buffered channel too
+	//   hub <- topic // the topic now accepts broadcasts
+	//   hub <- RemoveTopic(topic) // the topic channel will be closed and all connections will be disconnected from it.
+	//   close(hub) // this will close all topics and connections
+	//
+	// See also the New example.
+	Hub chan interface{}
+	// The Topic channel is used to broadcast messages to connections.
+	// To connect to a Topic, send a Conn to the channel:
+	//
+	//   topic := make(Topic) // you can use a buffered channel too
+	//   // register to a hub
+	//   conn := make(Conn)
+	//   topic <- conn // the connection will receive all messages broadcasted to the topic
+	//   topic <- Disconnect(conn) // the connection will stop receiving messages from this topic
+	//
+	// See altso the New example.
+	Topic chan interface{}
+	// The Conn channel is used for receiving messages from the topics it's connected to.
+	//
+	//   conn := make(Conn) // you can use a buffered channel too
+	//   // connect to a registered topic
+	//   for message := range conn {
+	//     fmt.Println(message)
+	//   }
+	//
+	// See the examples for Topic and New for usage.
+	Conn chan interface{}
+	// The Disconnect type is a Conn that when sent to a Topic it tells that Topic to stop sending the given Conn
+	// messages. See the Topic and New examples for usage.
+	Disconnect Conn
+	// The RemoveTopic type is a Topic that when sent to a Hub it tells that Hub to close the given Topic. Broadcasting
+	// new messages won't work after this. See the Topic and New examples for usage.
 	RemoveTopic Topic
 
 	refCounter chan interface{}
